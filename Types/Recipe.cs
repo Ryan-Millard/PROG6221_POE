49f03c0dfeacc7f3fff7660569475ca790b03b2a
Ryan-Millard <millardryandevon@gmail.com>
2024-04-17 19:13:39 +0200

    Added Recipe type that contains a name, array of Ingredients, and an array of steps (as strings) as well as a Scale method

--- Types/Recipe.cs
+++ Types/Recipe.cs
@@ -1,19 +1,34 @@
-namespace Types
-{
-	namespace Recipe
-	{
-		public class Recipe
-		{
-			public string Name { get; set; }
-			public Ingredient[] Ingredients { get; set; }
-			public byte NumSteps { get; set; }
+using System.Linq;
 
-			public Recipe(string name, byte numIngredients, Ingredient[] ingredients, byte numSteps)
-			{
-				Name = name;
-				Ingredients = ingredients;
-				NumSteps = numSteps;
-			}
+namespace Types.Recipe
+{
+	public class Recipe
+	{
+		public string Name { get; set; }
+		public Ingredient[] Ingredients { get; set; }
+		public string[] Steps { get; set; }
+
+		public Recipe(string name, Ingredient[] ingredients, string[] steps)
+		{
+			Name = name;
+			Ingredients = ingredients;
+			Steps = steps;
+		}
+
+		public Recipe Scale(float scaleFactor)
+		{
+			var scaledIngredients = Ingredients.Select(ingredient =>
+						new Ingredient(ingredient.Name, ingredient.Quantity * scaleFactor, ingredient.Unit)
+					).ToArray();
+
+			return new Recipe(Name, scaledIngredients, Steps);
+		}
+
+		public void Deconstruct(out string name, out Ingredient[] ingredients, out string[] steps)
+		{
+			name = Name;
+			ingredients = Ingredients;
+			steps = Steps;
 		}
 	}
 }
