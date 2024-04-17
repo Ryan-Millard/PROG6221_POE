00286f749cb71abc9f85b10ffad51329f568f782
Ryan-Millard <millardryandevon@gmail.com>
2024-04-17 19:12:42 +0200

    Created Ingredient data type to store the name, quantity, and unit of measurement of a single ingredient

--- Types/Ingredient.cs
+++ Types/Ingredient.cs
@@ -1,21 +1,27 @@
-namespace Types
+namespace Types.Recipe
 {
-	namespace Recipe
+	public class Ingredient
 	{
-		public class Ingredient
-		{
-			public string Name { get; set; }
-			public float Quantity { get; set; }
-			public string Unit { get; set; }
+		public string Name { get; set; }
+		public float Quantity { get; set; }
+		public string Unit { get; set; }
 
-			public Ingredient(string name, float quantity, string unit)
-			{
-				Quantity = (quantity != 0)
-						? quantity
-						: throw new ArgumentOutOfRangeException("Ingredient quantity may not be 0 (zero).");
-				Name = name;
-				Unit = unit;
-			}
+		public Ingredient(string name, float quantity, string unit)
+		{
+			Quantity = (quantity != 0)
+					? quantity
+					: throw new ArgumentOutOfRangeException("Ingredient quantity may not be 0 (zero).");
+			Name = name;
+			Unit = unit;
+		}
+
+		public override string ToString() { return $"{Name} ({Quantity} {Unit})"; }
+
+		public void Deconstruct(out string name, out float quantity, out string unit)
+		{
+			name = Name;
+			quantity = Quantity;
+			unit = Unit;
 		}
 	}
 }
