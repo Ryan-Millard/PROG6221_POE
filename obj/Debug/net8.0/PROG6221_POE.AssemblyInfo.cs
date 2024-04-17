bf4389bc2a1ed9a16f5d2f684a6a240129e1b748
Ryan-Millard <millardryandevon@gmail.com>
2024-04-17 19:19:29 +0200

    Cleanup commit

--- obj/Debug/net8.0/PROG6221_POE.AssemblyInfo.cs
+++ obj/Debug/net8.0/PROG6221_POE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROG6221_POE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83985bfc67048868a73a978a6b629e02c2d0ebc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62cda1f085b3367e8911c092b5f8bcf9e78c9fef")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROG6221_POE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROG6221_POE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
