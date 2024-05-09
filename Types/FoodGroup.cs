1e02f63d452745621ff633fc09b83b05243d9955
Ryan-Millard <millardryandevon@gmail.com>
2024-05-09 16:21:22 +0200

    Created FoodGroup enum

---
+++ Types/FoodGroup.cs
@@ -0,0 +1,15 @@
+namespace Types.Recipe
+{
+	public enum FoodGroup
+	{
+		Starchy = "starch",
+		FruitsAndVegetables = "fruits and vegetables",
+		Legumes = "legumes",
+		Protein = "protein",
+		Dairy = "dairy",
+		FatsAndOil = "fats and oil",
+		Water = "water",
+		other = "other"
+	}
+
+}
