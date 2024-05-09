19713632d23764689705753298c9f90bd1ef7bc7
Ryan-Millard <millardryandevon@gmail.com>
2024-05-09 16:23:36 +0200

    Added FoodGroup named Category and float named Quantity to Types/Ingredient.cs

--- Types/Ingredient.cs
+++ Types/Ingredient.cs
@@ -5,8 +5,10 @@ namespace Types.Recipe
 		public string Name { get; set; }
 		public float Quantity { get; set; }
 		public string Unit { get; set; }
+		public float Calories { get; set; }
+		public FoodGroup Category { get; set; }
 
-		public Ingredient(string name, float quantity, string unit)
+		public Ingredient(string name, float quantity, string unit, float calories, FoodGroup category)
 		{
 			// ensure that the quantity is a natural number
 			Quantity = (quantity > 0)
@@ -14,19 +16,23 @@ namespace Types.Recipe
 					: throw new ArgumentOutOfRangeException("Ingredient quantity may not be less than or equal to 0 (zero).");
 			Name = name;
 			Unit = unit;
+			Calories = calories;
+			Category = category;
 		}
 
 		public override string ToString()
-		// simple ToString function
 		{
-			return $"{Name} ({Quantity} {Unit})";
+			return $"{Name} ({Quantity} {Unit}) ({Calories} calories) ({Category}))";
 		}
 
-		public void Deconstruct(out string name, out float quantity, out string unit)
+		public void Deconstruct(out string name, out float quantity, out string unit,
+								out float calories, out FoodGroup Category)
 		{
 			name = Name;
 			quantity = Quantity;
 			unit = Unit;
+			calories = Calories;
+			category = Category;
 		}
 	}
 }
