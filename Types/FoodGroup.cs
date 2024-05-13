201d4464b55f11661ebe7dd1266d8602875a0835
Ryan-Millard <millardryandevon@gmail.com>
2024-05-13 14:21:58 +0200

    Update Types/FoodGroup.cs - Add description and extension method

--- Types/FoodGroup.cs
+++ Types/FoodGroup.cs
@@ -1,15 +1,47 @@
+using System;
+using System.ComponentModel;
+using System.Reflection;
+
 namespace Types.Recipe
 {
-	private enum FoodGroup
+	public enum FoodGroup
 	{
-		Starchy = "starch",
-		FruitsAndVegetables = "fruits and vegetables",
-		Legumes = "legumes",
-		Protein = "protein",
-		Dairy = "dairy",
-		FatsAndOil = "fats and oil",
-		Water = "water",
-		other = "other"
+		[Description("starch")]
+		Starch,
+
+		[Description("fruits and vegetables")]
+		FruitsAndVegetables,
+
+		[Description("legumes")]
+		Legumes,
+
+		[Description("protein")]
+		Protein,
+
+		[Description("dairy")]
+		Dairy,
+
+		[Description("fats and oil")]
+		FatsAndOil,
+
+		[Description("water")]
+		Water,
+
+		[Description("other")]
+		Other
 	}
 
+	public static class FoodGroupExtensions
+	{
+		public static string GetDescription(this FoodGroup value)
+		{
+			FieldInfo? field = value.GetType().GetField(value.ToString());
+
+			if(field == null) return value.ToString();
+
+			var attribute = field.GetCustomAttribute(typeof(DescriptionAttribute)) as DescriptionAttribute;
+
+			return attribute == null ? value.ToString() : attribute.Description;
+		}
+	}
 }
