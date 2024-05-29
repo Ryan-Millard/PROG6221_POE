a12518bbbefff9767ff8f8f7f2e4e8fb41ce9e6a
Ryan-Millard <millardryandevon@gmail.com>
2024-05-29 19:36:30 +0200

    Moved source code to PROG6221_POE and created unit tests for it in PROG6221_POE.Tests

---
+++ PROG6221_POE.Tests/UnitTest1.cs
@@ -0,0 +1,35 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Types.Recipe;
+
+namespace RecipeTests
+{
+	[TestClass]
+	public class RecipeTests
+	{
+		[TestMethod]
+		public void TestTotalCaloriesCalculation()
+		{
+			// Arrange
+			var ingredients = new Ingredient[]
+			{
+				new Ingredient("Rice", 200, "grams", 240, FoodGroup.Starch),
+					new Ingredient("Chicken", 150, "grams", 330, FoodGroup.Protein),
+					new Ingredient("Broccoli", 100, "grams", 50, FoodGroup.FruitsAndVegetables)
+			};
+			var steps = new string[]
+			{
+				"Boil the rice.",
+				"Grill the chicken.",
+				"Steam the broccoli."
+			};
+			var recipe = new Recipe("Chicken and Rice", ingredients, steps);
+
+			// Act
+			var totalCalories = recipe.TotalCalories;
+
+			// Assert
+			Assert.AreEqual(620, totalCalories, "The total calories calculation is incorrect.");
+		}
+	}
+}
+
